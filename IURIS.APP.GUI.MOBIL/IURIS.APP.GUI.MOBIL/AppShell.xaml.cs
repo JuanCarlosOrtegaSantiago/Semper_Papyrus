664fb5ca3cc208b360fe9054ff94f017b47e7aef
Juan Carlos Ortega Santiago <tecprogjuancarlos@gmail.com>
2020-06-19 12:25:43 -0500

    utilizando nuevo masterdetail

---
+++ IURIS.APP.GUI.MOBIL/IURIS.APP.GUI.MOBIL/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace IURIS.APP.GUI.MOBIL
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IURIS.APP.GUI.MOBIL/IURIS.APP.GUI.MOBIL/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using IURIS.APP.GUI.MOBIL.Models;
+
+namespace IURIS.APP.GUI.MOBIL.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
