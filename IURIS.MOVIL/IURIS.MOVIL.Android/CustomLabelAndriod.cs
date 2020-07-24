793c45a404c2affe95ffeac0b7c185705e84ee94
Juan Carlos Ortega Santiago <tecprogjuancarlos@gmail.com>
2020-07-23 23:26:55 -0500

    Justificando texto de ley, cambiando iconos y diseño

---
+++ IURIS.MOVIL/IURIS.MOVIL.Android/CustomLabelAndriod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using IURIS.MOVIL.Droid;
+using IURIS.MOVIL.Modelos_y_clases;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly:ExportRenderer(typeof(CustomLabel),typeof(CustomLabelAndriod))]
+namespace IURIS.MOVIL.Droid
+{
+    public class CustomLabelAndriod:LabelRenderer
+    {
+        public CustomLabelAndriod(Context context):base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);   
+            if(Control != null)
+            {
+                Control.JustificationMode = JustificationMode.InterWord;
+            }
+        }
+    }
+}
---
+++ IURIS.MOVIL/IURIS.MOVIL.iOS/CustmLabelios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using IURIS.MOVIL.iOS;
+using IURIS.MOVIL.Modelos_y_clases;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly : ExportRenderer(typeof(CustomLabel),typeof(CustmLabelios) )]
+namespace IURIS.MOVIL.iOS
+{
+    public class CustmLabelios:LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control!=null)
+            {
+                Control.TextAlignment = UITextAlignment.Justified;
+            }
+        }
+    }
+}
