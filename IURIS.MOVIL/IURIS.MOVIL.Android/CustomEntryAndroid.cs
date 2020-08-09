afc5f2ba57953785ecad211ed998d9d598589073
Juan Carlos Ortega Santiago <tecprogjuancarlos@gmail.com>
2020-08-09 15:21:54 -0500

    CorrijiendoInterfaz en entry

---
+++ IURIS.MOVIL/IURIS.MOVIL.Android/CustomEntryAndroid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using IURIS.MOVIL.Droid;
+using IURIS.MOVIL.Modelos_y_clases;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryAndroid))]
+
+namespace IURIS.MOVIL.Droid
+{
+    public class CustomEntryAndroid:EntryRenderer
+    {
+        public CustomEntryAndroid(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetColor(global::Android.Graphics.Color.Transparent);
+                Control.SetBackgroundDrawable(gd);
+            }
+
+        }
+    }
+}
---
+++ IURIS.MOVIL/IURIS.MOVIL.iOS/CustomEntryIOS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using IURIS.MOVIL.iOS;
+using IURIS.MOVIL.Modelos_y_clases;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryIOS))]
+
+namespace IURIS.MOVIL.iOS
+{
+    public class CustomEntryIOS:EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            //if (Control != null)
+            //{
+            //    GradientDrawable gd = new GradientDrawable();
+            //    gd.SetColor(global::Android.Graphics.Color.Transparent);
+            //    Control.SetBackgroundDrawable(gd);
+            //}
+        }
+    }
+}
