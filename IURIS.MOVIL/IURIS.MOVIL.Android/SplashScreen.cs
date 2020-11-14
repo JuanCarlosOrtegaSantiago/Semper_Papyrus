b31eb844fb54fe363ee13874896c265c5933a1af
Juan Carlos Ortega Santiago <37173001+JuanCarlosOrtegaSantiago@users.noreply.github.com>
2020-11-14 09:07:58 -0600

    creacion de icon correcta

---
+++ IURIS.MOVIL/IURIS.MOVIL.Android/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace IURIS.MOVIL.Droid
+{
+    [Activity(Label = "IURIS", Icon = "@mipmap/icon", Theme = "@style/SplashTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashScreen: Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Thread.Sleep(500);
+            Finish();
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
