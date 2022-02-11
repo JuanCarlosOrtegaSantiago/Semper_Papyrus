d888dcb1b3a4e422d0845dad528aa7d7fc7b7152
Juan Calos Ortega Santiago <the_devil.jkx@hotmail.com>
2022-02-11 14:31:12 -0600

    cambio de icon

--- IURIS.MOVIL/IURIS.MOVIL.Android/Resources/Resource.designer.cs
+++ IURIS.MOVIL/IURIS.MOVIL.Android/Resources/Resource.designer.cs
@@ -40228,10 +40228,13 @@ namespace IURIS.MOVIL.Droid
 			public const int IURIS = 2131492866;
 			
 			// aapt resource value: 0x7F0C0003
-			public const int launcher_foreground = 2131492867;
+			public const int IURIS_adaptive_back = 2131492867;
 			
 			// aapt resource value: 0x7F0C0004
-			public const int launcher_IURIS = 2131492868;
+			public const int IURIS_adaptive_fore = 2131492868;
+			
+			// aapt resource value: 0x7F0C0005
+			public const int launcher_foreground = 2131492869;
 			
 			static Mipmap()
 			{
